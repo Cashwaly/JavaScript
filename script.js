3f43aacc21b5cc1bacf7a2d1adb0a41fc91eedde
Cashwaly <130985970+Cashwaly@users.noreply.github.com>
2023-05-03 12:18:45 +0200

    array

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-// Array erstellen
-let meinArray = ["Apfel", "Banane", "Orange", "Erdbeere"];
+let textListe = [];
 
-// Referenz auf HTML-Element holen
-let meinArrayDiv = document.getElementById("meinArray");
+function addText() {
+  let textInput = document.getElementById("textInput").value;
+  textListe.push(textInput);
 
-// Schleife durch das Array
-for (let i = 0; i < meinArray.length; i++) {
-  // Ein neues Element erstellen
-  let neuesElement = document.createElement("p");
-  // Den Text des Elements auf das Array-Element setzen
-  neuesElement.innerText = meinArray[i];
-  // Das Element zum HTML-Dokument hinzufügen
-  meinArrayDiv.appendChild(neuesElement);
+  let ausgabeListe = document.getElementById("ausgabeListe");
+  ausgabeListe.innerHTML = "";
+
+  // Textliste in der HTML darstellen
+  textListe.forEach(function(text, index) {
+    ausgabeListe.innerHTML += (index + 1) + ". " + text + "<br>";
+  });
+
+  // "Löschen"-Button anzeigen
+  let loeschenButton = document.getElementById("loeschenButton");
+  loeschenButton.style.display = "inline";
+}
+
+function loescheText() {
+  // Letzten Eintrag aus der Liste entfernen
+  textListe.pop();
+
+  let ausgabeListe = document.getElementById("ausgabeListe");
+  ausgabeListe.innerHTML = "";
+
+  // Textliste in der HTML darstellen
+  textListe.forEach(function(text, index) {
+    ausgabeListe.innerHTML += (index + 1) + ". " + text + "<br>";
+  });
+
+  // "Löschen"-Button ausblenden, wenn die Liste leer ist
+  let loeschenButton = document.getElementById("loeschenButton");
+  if (textListe.length == 0) {
+    loeschenButton.style.display = "none";
+  }
 }
